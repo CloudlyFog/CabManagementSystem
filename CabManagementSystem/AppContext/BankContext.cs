23ad062581de2bc70edd0c7d6e7c64ce7392fc2f
Maxim <cloudyfg@gmail.com>
2022-06-12 20:16:39 +0300

    added functional for banks. added tables of banks and operations in the database. created models of banks and operations.

---
+++ CabManagementSystem/AppContext/BankContext.cs
@@ -0,0 +1,15 @@
+﻿using CabManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CabManagementSystem.AppContext
+{
+    public class BankContext : DbContext
+    {
+        public BankContext(DbContextOptions<BankContext> options) : base(options) => Database.EnsureCreated();
+
+        public DbSet<BankModel> Banks { get; set; }
+        public DbSet<OperationModel> Operations { get; set; }
+
+
+    }
+}
--- CabManagementSystem/Models/BankModel.cs
+++ CabManagementSystem/Models/BankModel.cs
@@ -1,9 +1,32 @@
-﻿namespace CabManagementSystem.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CabManagementSystem.Models
 {
     public class BankModel
     {
         public Guid ID { get; set; } = Guid.NewGuid(); // id for identification in the database
         public Guid BankID { get; set; }
         public string BankName { get; set; } = string.Empty;
+        public int MembersAmount { get; set; }
+        public decimal AccountAmount { get; set; }
+
+        [NotMapped]
+        public UserModel? User { get; set; } = new();
+    }
+    public class OperationModel
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public Guid BankID { get; set; } = Guid.NewGuid();
+        public Guid ReceiverID { get; set; } = Guid.NewGuid();
+        public Guid SenderID { get; set; } = Guid.NewGuid();
+        public decimal TransferAmount { get; set; }
+        public StatusOperationCode OperationStatus { get; set; } = StatusOperationCode.Default;
+    }
+    public enum StatusOperationCode
+    {
+        Default = 100,
+        Successfull = 200,
+        Restricted = 300,
+        Error = 400,
     }
 }
